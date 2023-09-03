d9a1ac52ec7c86d048860165bccc1d05d42f5b6e
PedroZappa <pedrogzappa@gmail.com>
2023-09-03 14:50:54 +0100

    fix colors.lua : needs extensive re-work to be useful

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,11 +1,12 @@
 -- My colors
 function ColorScheme(color)
+    vim.cmd.colorscheme(color)
 
 	-- Set Background Transparency
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
 	-- Set float Transparency
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
 end
-
+    
 ColorScheme()
 
